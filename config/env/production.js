8796b153452563f2a0d0220e45a1e6564ed0c917
Muhammed <aapozaid@gmail.com>
2019-04-24 11:37:48 +0200

    openVidu production credentials

--- config/env/production.js
+++ config/env/production.js
@@ -21,7 +21,8 @@
 
 module.exports = {
 
-
+  OPENVIDU_URL : 'https://localhost:4443', // 'https://192.168.43.167:4443'//'https://kurento-hugathome.dev.oniabsis.com:443'//'https://' + location.hostname + ':4443/';
+  OPENVIDU_SECRET : 'MY_SECRET',
   /**************************************************************************
   *                                                                         *
   * Tell Sails what database(s) it should use in production.                *
