9b0688f967092290a01ee30cf6d5ca6cf725daff
Muhammed <aapozaid@gmail.com>
2019-05-01 20:17:36 +0200

    file upload

--- api/policies/isLoggedIn.js
+++ api/policies/isLoggedIn.js
@@ -4,7 +4,7 @@ module.exports = async function (req, res, proceed) {
 
 
 
-  if(!req.headers['x-access-token'] && !req.query.token) {return res.sendStatus(400);}
+  if(!req.headers['x-access-token'] && !req.query.token) {return res.sendStatus(401);}
   jwt.verify(req.headers['x-access-token'] || req.query.token , sails.config.globals.APP_SECRET, (err, decoded) => {
     if(err){
       console.log('error ', err);
