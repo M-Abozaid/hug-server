350897ebc85080e211b4483d14565a543a8fad90
Muhammed <aapozaid@gmail.com>
2020-03-14 23:50:55 +0200

    mark as accepted

--- config/security.js
+++ config/security.js
@@ -35,8 +35,6 @@ module.exports.security = {
       'http://localhost:8080',
       'http://localhost:8100',
       'http://localhost:4200',
-      'http://192.168.1.9:3000',
-      'http://192.168.1.9:8100'
     ],
     allowCredentials: true,
     // allowAnyOriginWithCredentialsUnsafe: true,
