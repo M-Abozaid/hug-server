6b124e9125bfe878e7c4410b1af2516159c5aff1
Muhammed <aapozaid@gmail.com>
2020-03-17 18:08:20 +0200

    remove extra ,

--- api/controllers/InviteController.js
+++ api/controllers/InviteController.js
@@ -133,7 +133,7 @@ function sendSmsWithSwisscom(phoneNumber, message) {
       return reject(e)
     });
     console.log('Siss come auth header  ', `${process.env.SMS_SWISSCOM_ACCOUNT}:${process.env.SMS_SWISSCOM_PASSWORD}`)
-    console.log('SISSCOME URI',  `https://messagingproxy.swisscom.ch:4300/rest/1.0.0/submit_sm/${process.env.SMS_SWISSCOM_ACCOUNT}`,)
+    console.log('SISSCOME URI',  `https://messagingproxy.swisscom.ch:4300/rest/1.0.0/submit_sm/${process.env.SMS_SWISSCOM_ACCOUNT}`)
     console.log('SWISSCOM JSON PAYLOAD..............')
     console.log(JSON.stringify(payload))
     request.write(JSON.stringify(payload));
