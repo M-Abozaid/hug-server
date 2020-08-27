8b535c93ddbb20a2edc234854766b240f7d03b67
Olivier Bitsch <olivier.b@iabsis.com>
2020-08-27 09:39:26 +0200

    [CHANGE] - rollback helpers as api now uses previous result style without pagination structure.

--- helpers/lib/lib.py
+++ helpers/lib/lib.py
@@ -8,9 +8,34 @@ import bcrypt, string, random
 import re
 import time
 
-api_token =
-api_id =
-api_url_base =
+## DEV
+api_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9AaWFic2lzLmNvbSIsInVzZXJuYW1lIjoiaW5mb0BpYWJzaXMuY29tIiwiaWQiOiI1ZTZlMDU2OTA1NmRkMTUyMDg5OTI4NzIiLCJyb2xlIjoiYWRtaW4iLCJmaXJzdE5hbWUiOiJBZG1pbiIsImxhc3ROYW1lIjoiSUFCU0lTIiwiaWF0IjoxNTg0MjY4NzczfQ.8GItFBopfmThaWjvv-BivgeLMX40k3ZoP-ysJJfsqvM'
+api_id = '5e6e0569056dd15208992872'
+api_url_base = 'https://dev-medecin-hug-at-home.oniabsis.com/api/v1'
+
+## PROD HUG@Home
+#api_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9AaWFic2lzLmNvbSIsInVzZXJuYW1lIjoiaW5mb0BpYWJzaXMuY29tIiwiaWQiOiI1ZTZlYWZjNzUxZjExMzNjMmQyZTc1OWUiLCJyb2xlIjoiYWRtaW4iLCJmaXJzdE5hbWUiOiJPbGl2aWVyIiwibGFzdE5hbWUiOiJCSVRTQ0giLCJpYXQiOjE1ODQzMTI0NTB9.J82su85BPIEswHePg3mbwRZVCba4u5GLRIYz5BqU1QU'
+#api_id = '5e6eafc751f1133c2d2e759e'
+#api_url_base = 'https://hug-at-home.ch/api/v1'
+
+## PROD DOCTEUR@Home
+#api_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9AaWFic2lzLmNvbSIsImlkIjoiNWU3N2NhYmRjMzQ0ZWUyYjc1ODg0ZjhkIiwicm9sZSI6ImFkbWluIiwiZmlyc3ROYW1lIjoiSWFic2lzIiwibGFzdE5hbWUiOiJBZG1pbiIsImlhdCI6MTU4NDkwODk5N30.VypI0tSCWFXwkSOkyTffKr2Jd1J-bvhpzWUjqHb3CQk'
+#api_id = '5e77cabdc344ee2b75884f8d'
+#api_url_base = 'https://docteur-at-home.ch/api/v1'
+
+## PROD HCW@Home (Demo)
+#api_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9AaWFic2lzLmNvbSIsInVzZXJuYW1lIjoiaW5mb0BpYWJzaXMuY29tIiwiaWQiOiI1ZWFhYzJkOGRhOGI0ZjI2YmUzZmE2OTgiLCJyb2xlIjoiYWRtaW4iLCJmaXJzdE5hbWUiOiJBZG1pbiIsImxhc3ROYW1lIjoiSUFCU0lTIiwicGhvbmVOdW1iZXIiOiIiLCJpYXQiOjE1ODgyNDk2MjF9.xngg93FewFFFGZ_L0Ahp1VjqRf1P4Uxd5WcqKgEpqn4'
+#api_id = '5eaac2d8da8b4f26be3fa698'
+#api_url_base = 'https://demo.hcw-at-home.com/api/v1'
+
+
+## PROD IMAD@Home
+#api_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGh1Zy1hdC1ob21lLmNoIiwidXNlcm5hbWUiOiJhZG1pbkBodWctYXQtaG9tZS5jaCIsImlkIjoiNWViM2I2NWU5ZWNhY2E0YmE2MmI4MDgxIiwicm9sZSI6ImFkbWluIiwiZmlyc3ROYW1lIjoiQURNSU4iLCJsYXN0TmFtZSI6IkhVRyIsInBob25lTnVtYmVyIjoiIiwiaWF0IjoxNTg4ODM1OTk5fQ.WBToRDHBwSIYSHp0-f1Eje7TMhE4g9cY_5jCWyFnTb4'
+#api_id = '5eb3b65e9ecaca4ba62b8081'
+#api_url_base = 'https://imad.hug-at-home.ch/api/v1'
+
+
+
 
 
 print("UPDATING URL (in 5 seconds): " + api_url_base)
@@ -24,7 +49,7 @@ class accounts():
            'id': api_id,
            'x-access-token': api_token}
         self.email = email
-        self.data = self.getAccountInfo(filter='role=doctor&email='+self.email)["results"]
+        self.data = self.getAccountInfo(filter='role=doctor&email='+self.email)
 
 
 
@@ -97,7 +122,7 @@ class queues():
            'id': api_id,
            'x-access-token': api_token}
         
-        self.data = self.getQueues()["results"]
+        self.data = self.getQueues()
 
     def getQueues(self):
 
@@ -125,7 +150,7 @@ class allDoctors():
         self.headers = {'Content-Type': 'application/json',
            'id': api_id,
            'x-access-token': api_token}
-        self.data = self.getAccountInfo(filter='role=doctor&limit=1000')["results"]
+        self.data = self.getAccountInfo(filter='role=doctor&limit=1000')
 
 
     def getAccountInfo(self, filter):
